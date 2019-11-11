63731d316c969c227b2803ec29c4f6fc1b46a89a
Fabian Böhm <fabian.boehm92@gmail.com>
2019-11-11 17:20:42 +0100

    Added basic components and Angular Material
    - Added component for Toolbar
    - Added component for Blockly Editor
    - Added component for Statement Overview
    - Added component for Statement Debugging
    - Added Angular Material to project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlocklyComponent } from './components/blockly/blockly.component';
+import { DebuggerComponent } from './components/debugger/debugger.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlocklyComponent,
+    DebuggerComponent,
+    OverviewComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

cc512fe7d884e634abd9b6a9c05ded22e2195fc9
antumin <m.vielberth@gmail.com>
2019-11-13 17:13:27 +0100

    Add data model for statement

---
+++ src/app/models/statemet.ts
@@ -0,0 +1,19 @@
+export interface Statement{
+    deploymentId?: string;
+    deploymentDependencies?: string[];
+    deploymentMode?: string;
+    eplStatement?: string;
+    name?: string;
+    blocklyXml?: string;
+    eventType?: boolean;
+}
+
+export class StatementDef{
+    deploymentId?: boolean;
+    deploymentDependencies?: boolean;
+    deploymentMode?: boolean;
+    eplStatement?: boolean;
+    name?: boolean;
+    blocklyXml?: boolean;
+    eventType?: boolean;
+}

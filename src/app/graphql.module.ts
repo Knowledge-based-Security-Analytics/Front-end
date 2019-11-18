287ffdd0f3eb5952dea6f3005ac1ccfcf74a65bf
antumin <m.vielberth@gmail.com>
2019-11-18 13:33:22 +0100

    Setup subscriptions

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,12 +1,37 @@
 import {NgModule} from '@angular/core';
-import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {ApolloModule, APOLLO_OPTIONS, Apollo} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
 import {InMemoryCache} from 'apollo-cache-inmemory';
+import { split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
 
-const uri = 'http://192.168.2.116:4000/'; // <-- add the URL of the GraphQL server here
+const uri = 'http://192.168.2.116:4000/';
+const websocketUri = 'ws://192.168.2.116:4000/graphql';
+
 export function createApollo(httpLink: HttpLink) {
+  const http = httpLink.create({
+    uri
+  });
+
+  const ws = new WebSocketLink({
+    uri: websocketUri,
+    options: {
+      reconnect: true
+    }
+  });
+
+  const link = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+    },
+    ws,
+    http,
+  );
+
   return {
-    link: httpLink.create({uri}),
+    link,
     cache: new InMemoryCache(),
   };
 }

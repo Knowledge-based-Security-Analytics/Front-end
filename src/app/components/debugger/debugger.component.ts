89abb6900d38b154528b7e1535081f32f7202445
antumin <m.vielberth@gmail.com>
2019-12-03 18:10:06 +0100

    Add MatList to debugger

--- src/app/components/debugger/debugger.component.ts
+++ src/app/components/debugger/debugger.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { EventStreamService } from './../../services/event-stream.service';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
+import { MatList } from '@angular/material/list';
 
 @Component({
   selector: 'app-debugger',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./debugger.component.css']
 })
 export class DebuggerComponent implements OnInit {
+  @ViewChildren(MatList) lists: QueryList<any>;
 
-  constructor() { }
+  public logs: string[] = [];
+  public events: string[] = ['sdfasfd', 'asdfasfd', 'Oct 11 22:14:15 su: \'su root\' failed for lonvick on /dev/pts/8', 'asdfasfd'];
+  public incidents: string[] = [];
+
+  constructor(private eventStreamService: EventStreamService) {
+    let i = 0;
+    this.eventStreamService.subscribeTopic('test').subscribe(event => {
+      console.log(event);
+      this.logs.unshift(i + event.jsonString);
+      i++;
+      console.log(this.lists);
+    });
+  }
 
   ngOnInit() {
+    console.log(this.lists);
   }
 
 }

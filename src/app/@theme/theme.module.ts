75aa132bc160e575f4b01092985e73bef1cca57c
Fabian Böhm <fabian.boehm92@gmail.com>
2020-03-19 10:37:28 +0100

    Add a first set of necessary pipes

--- src/app/@theme/theme.module.ts
+++ src/app/@theme/theme.module.ts
@@ -1,11 +1,14 @@
-import { CapitalizePipe } from './pipes/capitalize.pipe';
 import { DEFAULT_THEME } from './styles/theme.default';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NbThemeModule } from '@nebular/theme';
+import { CapitalizePipe, ToUpperPipe, PluralPipe, RoundPipe } from './pipes';
 
 const PIPES = [
   CapitalizePipe,
+  ToUpperPipe,
+  PluralPipe,
+  RoundPipe,
 ];
 
 @NgModule({

6f4d9e65de493e73e2cda6699a926bd5b6cf8d94
antumin <m.vielberth@gmail.com>
2020-05-15 13:02:27 +0200

    Adjust model

--- src/app/shared/models/eplObjectRepresentation.ts
+++ src/app/shared/models/eplObjectRepresentation.ts
@@ -1,6 +1,7 @@
 export class Statement {
   type: 'schema' | 'pattern';
   name = '';
+  outputName: string;
   description = '';
   lastModified = '';
   blocklyXml = '';
@@ -24,6 +25,7 @@ class DeploymentProperties {
 }
 
 export class Schema extends Statement {
+  static readonly outputPrefix = 'viscep_event_atomic_';
   static readonly BASIC_ATTRIBUTES = [
     {name: 'complex', type: 'boolean'},
     {name: 'id', type: 'string'},
@@ -34,9 +36,21 @@ export class Schema extends Statement {
     {name: 'sources', type: 'object[]'}
   ];
 
+  private internalName = '';
   type: 'schema' = 'schema';
   complexEvent = false;
   attributes: {name: string, type: string}[] = Schema.BASIC_ATTRIBUTES;
+  set name(name: string){
+    if (name) {
+      this.internalName = name.replace(Schema.outputPrefix, '');
+    }
+  }
+  get name(){
+    return this.internalName;
+  }
+  get outputName() {
+    return Schema.outputPrefix + this.name;
+  }
 
   constructor(name?: string) {
     super();
@@ -56,11 +70,24 @@ export class ConditionedEvent {
 }
 
 export class Pattern extends Statement {
+  static readonly outputPrefix = 'viscep_event_complex_';
   type: 'pattern' = 'pattern';
   outputSchema: Schema = new Schema();
   outputAttributes: {inputAttribute: string, outputAttribute: string}[] = [];
   events: IEventAlias[] = [];
   eventSequence: (ConditionedEvent | PatternDefinition)[] = [];
+  internalName = '';
+  set name(name: string){
+    if (name) {
+      this.internalName = name.replace(Pattern.outputPrefix, '');
+    }
+  }
+  get name(){
+    return this.internalName;
+  }
+  get outputName(){
+    return Pattern.outputPrefix + this.name;
+  }
 }
 
 export interface IEventAlias {

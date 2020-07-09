435585373338e7ddd1253b397dd991247b99ae44
antumin <m.vielberth@gmail.com>
2020-07-09 17:13:12 +0200

    fix debugger subscribing to wrong topics

--- src/app/pages/overview/components/debugger/debugger.component.ts
+++ src/app/pages/overview/components/debugger/debugger.component.ts
@@ -37,26 +37,35 @@ export class DebuggerComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   private async subscribeTopics() {
-    this.subscribeTopic(this.statement, 2);
+    this.subscribeTopic(this.statementService.getOutputTopic(this.statement), 2);
 
     let statementIDs: string[] = this.statement.deploymentProperties.dependencies;
     // tslint:disable-next-line: prefer-for-of
     for (let i = 0; i < statementIDs.length; i++) {
       const statementToSubscribe = this.statementService.getStatement(statementIDs[i]);
-      if (statementToSubscribe.deploymentProperties.dependencies && statementToSubscribe.deploymentProperties.dependencies.length > 0) {
-        statementIDs = statementIDs.concat(statementToSubscribe.deploymentProperties.dependencies);
-        this.subscribeTopic(statementToSubscribe, 1);
+      if (!statementToSubscribe) {
+        continue;
+      }
+      const parentPatterns = this.statementService.getStatementsWithOutput(statementToSubscribe.outputName);
+      if (parentPatterns && parentPatterns.length > 0) {
+        parentPatterns.forEach(parentPattern => {
+          statementIDs = statementIDs.concat(parentPattern.deploymentProperties.dependencies.map(dependency => {
+            if (!statementIDs.includes(dependency)) {
+              return dependency;
+            }
+          }));
+        });
+        this.subscribeTopic(statementToSubscribe.outputName, 1);
         this.eventSubscribed[1] = true;
       } else {
-        this.subscribeTopic(statementToSubscribe, 0);
+        this.subscribeTopic(statementToSubscribe.outputName, 0);
         this.eventSubscribed[0] = true;
       }
     }
   }
 
-  private subscribeTopic(statement: Statement, position: number) {
-    const topic: string = this.getOutputTopic(statement);
-    // console.log('subscribing to: ' + topic);
+  private subscribeTopic(topic: string, position: number) {
+    console.log('subscribing to ' + topic + ' in column ' + position);
     this.subscriptions.push(this.eventStreamService.subscribeTopic(topic).subscribe((event) => {
       // console.log(event);
       const parsedEvent = JSON.parse(event.jsonString);
@@ -71,17 +80,6 @@ export class DebuggerComponent implements OnInit, OnChanges, OnDestroy {
     this.events = [[], [], []];
     this.eventSubscribed = [false, false, true];
   }
-
-  private getOutputTopic(statement: Statement): string {
-    let topic: string = statement.outputName;
-    if (statement.deploymentProperties.eplStatement.includes('@KafkaOutput')) {
-      topic = statement.deploymentProperties.eplStatement.match(/@KafkaOutput\('.*?'\)/)[0];
-      topic = topic
-        .slice(14, -2);
-    }
-    return topic;
-  }
-
   mouseoverEvent(id: number) {
     this.resetHighlights();
     this.highlightEvents(id);
